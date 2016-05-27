230b315c440ff5d76a58c076262ddb6f7fd03344
Irina Rabinovich <webmaza75@gmail.com>
2016-05-27 21:26:01 +0300

    lesson1

--- hello-npm.js
+++ hello-npm.js
@@ -1,8 +1,7 @@
-var	ansi = require('ansi');
-var	cursor = ansi(process.stdout);
+var	ansi = require('ansi'),
+    cursor = ansi(process.stdout);
 cursor
     .red()                 // Красный шрифт в консоли
     .bg.grey()             // Серый фон под текстом
     .write('Hello World!'); // Вывод сообщения в консоль
 cursor.beep();
-cursor.reset(); // сброс настроек курсора в консоли
